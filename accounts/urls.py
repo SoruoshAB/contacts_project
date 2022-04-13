791e8fe43bf0f3da40a0e5a90fb53aacf00360d6
Soroush <sorushabdollahi75@gmail.com>
2022-04-14 02:53:34 +0430

    add urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register/', views.UserRegister.as_view()),
+    path('register/', views.UserRegister.as_view(), name='register'),
+    path('logout/', views.Logout.as_view(), name='logout'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('user-update/', views.UserUpdate.as_view(), name='user-update'),
+    path('password-change/', views.UserPasswordChange.as_view(), name='password-change'),
+    path('', views.Users.as_view(), name='users'),
 ]
